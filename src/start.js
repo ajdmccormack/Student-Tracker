d26c925605b245d760b6a7984a950caf9d50f724
Colo553 <connor.langlois@gmail.com>
2017-02-14 12:11:10 -0500

    Start

---
+++ src/start.js
@@ -0,0 +1,9 @@
+const API_KEY = 'AIzaSyD0kTBtlNzVae3u1LYcjZKrre563mcxsVo';
+const CLIENT_ID = '627343722694-fni1on670josisrndul45j23n0bim9a5.apps.googleusercontent.com';
+const MAIN_SPREADSHEET_ID = '1IIjBsBJrkgaGORKvsXruJpk-6xKC3clPnW_-AyqpUMw';
+const FORM_SPREADSHEET_ID = '1bJRdFCGP2yocIVXw6ewsDBALeUkYfrVoorf5DnBpAc4';
+const SHEET = 'Sheet1';
+const ASSIGNMENT_ID = '~';
+const ASSESSMENT_ID = '`';
+
+document.body.appendChild(Object.assign(document.createElement('script'), {src: 'https://cdn.rawgit.com/Colo553/Student-Tracker/5a283ba0/src/main.js', defer: 'defer', async: 'async'}));
