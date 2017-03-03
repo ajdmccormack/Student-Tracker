8c094c43c79fa3d6c74c27a456ce8bf1bf0d70a8
Colo553 <connor.langlois@gmail.com>
2017-03-03 14:59:18 -0500

    Changed order of constants (prompt)

--- src/start.js
+++ src/start.js
@@ -3,8 +3,8 @@ const CLIENT_ID = '627343722694-fni1on670josisrndul45j23n0bim9a5.apps.googleuser
 const MAIN_SPREADSHEET_ID = '1IIjBsBJrkgaGORKvsXruJpk-6xKC3clPnW_-AyqpUMw';
 const FORM_SPREADSHEET_ID = '1bJRdFCGP2yocIVXw6ewsDBALeUkYfrVoorf5DnBpAc4';
 const SHEET = 'Sheet1';
+const PROMPT_PERIOD = 'In which class period are you? (1, 2, 4, 5, 7)';
 const ASSIGNMENT_ID = '~';
 const ASSESSMENT_ID = '`';
-const PROMPT_PERIOD = 'In which class period are you? (1, 2, 4, 5, 7)';
 
 document.body.appendChild(Object.assign(document.createElement('script'), {src: 'https://rawgit.com/Colo553/Student-Tracker/master/src/main.js', defer: 'defer', async: 'async'}));
