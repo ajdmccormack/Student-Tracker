750702830c84dc767e0677d2e2c554659a743328
Colo553 <connor.langlois@gmail.com>
2017-03-13 09:55:32 -0400

    Removed constants

--- beta/main.js
+++ beta/main.js
@@ -1,16 +1,5 @@
 'use strict';
 
-const API_KEY = 'AIzaSyD0kTBtlNzVae3u1LYcjZKrre563mcxsVo';
-const CLIENT_ID = '627343722694-fni1on670josisrndul45j23n0bim9a5.apps.googleusercontent.com';
-const MAIN_SPREADSHEET_ID = '1IIjBsBJrkgaGORKvsXruJpk-6xKC3clPnW_-AyqpUMw';
-const FORM_SPREADSHEET_ID = '1bJRdFCGP2yocIVXw6ewsDBALeUkYfrVoorf5DnBpAc4';
-const TEACHER_EMAIL = 'amccormack@scarboroughschools.org';
-const SHEET = 'Sheet1';
-const PROMPT_PERIOD = 'In which class period are you? (1, 2, 4, 5, 7)';
-const ASSIGNMENT_ID = '~';
-const ASSESSMENT_ID = '`';
-const CLONE_ID = '^';
-
 class Drive {}
 
 Drive.Files = class {
